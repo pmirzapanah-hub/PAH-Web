7983690dd5c32fde0687902c3fcd78b9b80fdffe
pmirzapanah-hub <p.mirzapanah@gmail.com>
2026-03-08 14:17:29 +1000

    Migrate server.js to ES module syntax

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "public")));
 
